f5da11f24334b75327d8d1697c8fd622778fcaba
Rian Nito <ryanitovercosa@gmail.com>
2026-05-30 12:22:01 -0300

    rota certa

--- src/main/java/com/estudarFatec/rian/controller/MensagemController.java
+++ src/main/java/com/estudarFatec/rian/controller/MensagemController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/mensagens")
+@RequestMapping("/mensagem")
 public class MensagemController {
 
     @Autowired
